260e89210e4d3007c891b053d0c68e88df6890f5
Vannguyen2804 <dinhvan2804@gmail.com>
2023-02-05 14:06:36 +0700

    init project

---
+++ SpringWebApp/src/main/java/com/simple/app/SpringWebApp/SpringWebAppApplication.java
@@ -0,0 +1,27 @@
+package com.simple.app.SpringWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppApplication.class, args);
+	}
+	
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+    
+    @GetMapping("/test")
+    public String test(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+
+}
